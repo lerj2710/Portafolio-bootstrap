b2cdded3ef03894e082e7f88a7c72322c11b3f39
luis rodriguez <kike.lerj2710@gmail.com>
2021-10-06 10:04:55 -0500

    corrigiendo la barra de navegacion

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,7 @@
-/**
-* Template Name: Personal - v4.5.0
-* Template URL: https://bootstrapmade.com/personal-free-resume-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
-  /**
-   * Easy selector helper function
-   */
   const select = (el, all = false) => {
     el = el.trim()
     if (all) {
